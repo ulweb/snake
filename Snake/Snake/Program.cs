46bc825b378b3b10b0d99c7f201bdba035ca3952
Mr. Nigmatullin <nigkam@mail.ru>
2015-10-12 20:19:23 +0300

    1

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,27 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // Console.WriteLine("Hello World");
+            
+            int x1 = 2;
+            int y1 = 3;
+            char sym1 = '$';
+
+            Draw(x1,y1,sym1);
+
+            int x2 = 5;
+            int y2 = 7;
+            char sym2 = '&';
+
+            Draw(x2, y2, sym2);
+
             Console.ReadLine();
+
+        }
+        static void Draw(int x, int y, char sym) {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+            
         }
     }
 }
